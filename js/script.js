01083ff1290f9194c2fb238c4fe053cc67b1e855
Martin <mmaina290@gmail.com>
2023-11-01 08:38:13 +0300

    js login/register

--- js/script.js
+++ js/script.js
@@ -12,5 +12,9 @@ const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 
 registerLink.addEventListener('click', () => {
-    
+    wrapper.classList.add('active');
+})
+
+loginLink.addEventListener('click', () => {
+    wrapper.classList.remove('active');
 })
